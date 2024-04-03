5c5a09dab482d6e6aa7d7995ca0e27b356d81c9e
Samuel Nwoko <sahmienwoko@gmail.com>
2024-04-03 16:15:40 -0400

    initial comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: `<h1>Hello world!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world-app';
+  title = 'homes';
 }
